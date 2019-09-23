223e480df8625e2df3957de3b792fa718165648f
vomvoru <qkrcjfgus3@gmail.com>
2019-09-23 12:44:51 +0900

    add blob link

--- scripts/jscpd.js
+++ scripts/jscpd.js
@@ -11,6 +11,7 @@ const ROOT = path.resolve(__dirname, '..');
 const TEMP_DIR_NAME = '.temp';
 const TEMP = path.resolve(ROOT, `./${TEMP_DIR_NAME}`);
 
+
 const execWithDebug = async (...args) => {
   debug(args[0]);
   const { stdout, stderr } = await exec(...args);
@@ -21,7 +22,7 @@ const execWithDebug = async (...args) => {
   return stdout;
 }
 
-async function createJscpd({ cloneUrl, branchName, sha }) {
+async function createJscpd({ cloneUrl, branchName, blobs_url }) {
   // await execWithDebug(`rm -rf .jscpd`, { cwd: ROOT });
   await execWithDebug(`rm -rf ${TEMP_DIR_NAME}`, { cwd: ROOT });
   await execWithDebug(`mkdir -p ${TEMP_DIR_NAME}`, { cwd: ROOT });
@@ -60,29 +61,33 @@ async function createJscpd({ cloneUrl, branchName, sha }) {
   await cpd.detectInFiles(config.files);
 
   debug('get md file');
-  const md = fs.readFileSync(path.resolve(TEMP, 'report', 'index.md'), 'utf8')
+  let md = fs.readFileSync(path.resolve(TEMP, 'report', 'index.md'), 'utf8')
 
   await execWithDebug(`rm -rf ${TEMP_DIR_NAME}`, { cwd: ROOT });
 
+  md = md.replace(/\/usr\/src\/app\/\.temp\//g, blobs_url);
+  md = md.replace(/ \[(\d+)\:\d+ \- (\d+)\:\d+\]/g,(match, startLineNumber, endLineNumber) => `#L${startLineNumber}-L${endLineNumber}`);
+  md = md.replace(new RegExp(`${escape(blobs_url)}([^\n]+)`, 'g'), (match, path) => `[${path}](${match})`);
+  
   return md;
 }
 
 const handlePullCreated = async (event, owner, repo) => {
-  const { number } = event;
+  const { number, repository: { blobs_url } } = event;
   const options = { owner, repo, pull_number: number };
 
   debug('create oldJscpdMd');
   const oldJscpdMd = await createJscpd({
     cloneUrl: event.repository.clone_url,
     branchName: event.pull_request.base.ref,
-    sha: event.pull_request.base.sha,
+    blobs_url: blobs_url.replace('{/sha}', `/${sha}`)
   });
 
   debug('create newscpdMd');
   const newscpdMd = await createJscpd({
     cloneUrl: event.repository.clone_url,
     branchName: event.pull_request.head.ref,
-    sha: event.pull_request.head.sha,
+    blobs_url: blobs_url.replace('{/sha}', `/${sha}`)
   });
 
   debug('getBody');
@@ -111,6 +116,11 @@ ${oldJscpdMd}
 
 ${newscpdMd}`
 }
+
+const escape = function(string) {
+  return string.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+};
+
 
 const handleSynchronize = handlePullCreated;
 
